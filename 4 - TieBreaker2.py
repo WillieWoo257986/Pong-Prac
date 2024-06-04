fe9504606194975d64a6ef1ed1ac2ddef7bf4c8c
William <williamkulcsar@gmail.com>
2024-06-04 20:46:54 +1000

    try again

---
+++ 4 - TieBreaker2.py
@@ -0,0 +1,28 @@
+def InitGen(s,n):
+    possibleCombos = []
+
+    for a in range(s):
+        if(n >= 2):
+            for b in range(s):
+                if(n >= 3):
+                    for c in range(s):
+                        if(n >= 4):
+                            for d in range(s):
+                                if(n >= 5):
+                                    for e in range(s):
+                                        if(n >= 6):
+                                            for f in range(s):
+                                                possibleCombos.append([a+1,b+1,c+1,d+1,e+1,f+1])
+                                        else:
+                                            possibleCombos.append([a+1,b+1,c+1,d+1,e+1])
+                                else:
+                                    possibleCombos.append([a+1,b+1,c+1,d+1])
+                        else:
+                            possibleCombos.append([a+1,b+1,c+1])
+                else:
+                    possibleCombos.append([a+1,b+1])
+        else:
+            possibleCombos.append([a+1])
+    return(possibleCombos)
+
+InitGen(3,6)
